46bfdff8ab4b6140ec8a08a2a3fa144037bb805d
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-04-20 11:14:51 +0200

    RED Purchase Test
    Purchase describes the purchase of a product. The Purchase is
    characterized by a product object and the quantity of product purchased.

--- Sales_Taxes_Problem/junit/junit/ProductTest.java
+++ Sales_Taxes_Problem/junit/junit/ProductTest.java
@@ -29,4 +29,23 @@ class ProductTest {
 		Product p = new Product("book", -12.49);
 		assertEquals("Error price 0.0", p.getDescription() + " " + p.getPrice());
 	}
+	
+	@Test
+	void testBookPurchace() {
+		Purchase a = new Purchase(2, "book", 12.49);	
+		assertEquals("2 book 12.49", a.getQuantity() + " " + a.getDescription() + " " + a.getPrice());
+	}
+	/*
+	@Test
+	void testMusicPurchace() {
+		Purchase a = new Purchase(1, "music CD", 14.99);
+		assertEquals("1 music CD 14.99", a.getQuantity() + " " + a.getDescription() + " " + a.getPrice());
+	}
+	/*
+	@Test
+	void testBarPurchace() {
+		Purchase a = new Purchase(1, "chocolate bar", 0.85);
+		assertEquals("1 chocolate bar 0.85", a.getQuantity() + " " + a.getDescription() + " " + a.getPrice());	
+	}
+	*/
 }
