17276f7c01ebc394bbc8c57be355e3a7878c7858
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-04-20 11:46:45 +0200

    GREEN Negative quantity Test
    If a negative quantity is inserted, it is broughr to 1 (presumed error).

--- Sales_Taxes_Problem/src/salesTaxesProgram/Purchase.java
+++ Sales_Taxes_Problem/src/salesTaxesProgram/Purchase.java
@@ -6,6 +6,10 @@ public class Purchase extends Product {
 	
 	public Purchase (int quantity, String description, double price) {
 		super(description, price);
+		if (quantity <= 0) {
+			this.quantity = 1;
+			return ;
+		} 
 		this.quantity = quantity;
 	}
 	
