6d082519522506bca2ceeeb4e4bea1edae3642d5
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-04-20 11:02:37 +0200

    RED Product with Negative Price Test
    A product cannot have a negative price.

--- Sales_Taxes_Problem/junit/junit/ProductTest.java
+++ Sales_Taxes_Problem/junit/junit/ProductTest.java
@@ -11,17 +11,22 @@ class ProductTest {
 		Product p = new Product("book", 12.49);
 		assertEquals("book 12.49", p.getDescription() + " " + p.getPrice());
 	}
-	/*
+	
 	@Test
 	void testMusic() {
 		Product p = new Product("music CD", 14.99);
 		assertEquals("music CD 14.99", p.getDescription() + " " + p.getPrice());
 	}
-	/*
+	
 	@Test
 	void testBar() {
 		Product p = new Product("chocolate bar", 0.85);
 		assertEquals("chocolate bar 0.85", p.getDescription() + " " + p.getPrice());
 	}
-	*/
+	
+	@Test
+	void testNegativePrice() {
+		Product p = new Product("book", -12.49);
+		assertEquals("Error price 0.0", p.getDescription() + " " + p.getPrice());
+	}
 }
--- Sales_Taxes_Problem/src/salesTaxesProgram/Product.java
+++ Sales_Taxes_Problem/src/salesTaxesProgram/Product.java
@@ -5,7 +5,7 @@ public class Product {
 	private String description;
 	private double price;
 	
-	public Product(String description, double price) {
+	public  Product(String description, double price) {
 		this.description = description;
 		this.price = price;
 	}
