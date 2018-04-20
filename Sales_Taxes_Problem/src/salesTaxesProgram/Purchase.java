54ab67e8af17e1667fa288bbcf1f41e181a36d42
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-04-20 12:19:54 +0200

    GREEN Parse String to Create Purchase object
    Parse very simple that ignores the penultimate token "at" and passes to
    the constructor Purchase the quantity to the first token of the string,
    the description to the second token of the string and the price last token of the String.
    Any word between the quantity and the penultimate token "at" is part of
    the description.
    METHOD LIMIT: doesn't check the format of the string.

--- Sales_Taxes_Problem/src/salesTaxesProgram/Purchase.java
+++ Sales_Taxes_Problem/src/salesTaxesProgram/Purchase.java
@@ -1,5 +1,8 @@
 package salesTaxesProgram;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class Purchase extends Product {
 
 	private int quantity;
@@ -15,6 +18,26 @@ public class Purchase extends Product {
 	
 	public int getQuantity() {
 		return this.quantity;
+	}
+	
+	public static Purchase parse(String inputLine) {
+		
+		ArrayList<String> in = new ArrayList<String>();
+	    StringTokenizer st = new StringTokenizer(inputLine, " ");
+		    
+	    while (st.hasMoreTokens())	    	
+	        in.add(st.nextToken());
+
+	    int quantity=Integer.parseInt(in.get(0));
+	    double price = Double.parseDouble(in.get(in.size()-1));
+	    
+	    String description = "";
+	     
+	    for(int i=1; i<in.size()-2; i++)
+	    	description = description.concat(" " + in.get(i));
+	    description = description.substring(1);	
+
+		return new Purchase(quantity, description, price);
 	}
 	
 }
