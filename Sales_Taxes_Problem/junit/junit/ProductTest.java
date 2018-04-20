2fa20eb3d930b55464a4b4019c0d9f1c31608744
MatteoCurti <matteo.curti1@studenti.unimi.it>
2018-04-20 17:49:03 +0200

    RED Medical Product Test
    The packet of headache pills isn't recognized as a medical product.

--- Sales_Taxes_Problem/junit/junit/ProductTest.java
+++ Sales_Taxes_Problem/junit/junit/ProductTest.java
@@ -146,4 +146,34 @@ class ProductTest {
 		assertEquals("1 imported bottle of perfume: 54.65", a.getQuantity() + " " + a.getDescription() + ": " + a.getPricePurchase());
 	}
 	
+	@Test
+	void testOutput() {
+		ArrayList<Purchase> al = new ArrayList<Purchase>();
+		al.add(Purchase.parse("1 imported box of chocolates at 10.00"));
+		al.add(Purchase.parse("1 imported bottle of perfume at 47.50"));
+		
+		Purchase p;
+		double totalPrice = 0.0;
+		double totalWithTax = 0.0;
+		for(int i = 0 ; i < al.size() ; i++) {
+			p = al.get(i); 
+			totalPrice += p.getPrice() * p.getQuantity();
+			totalWithTax += Double.parseDouble(p.getPricePurchase());
+		}
+		assertEquals("Sales Taxes: 7.65", "Sales Taxes: " + Round.format(totalWithTax - totalPrice));	
+		assertEquals("Total: 65.15", "Total: " + Round.format(totalWithTax));	
+	}
+	
+	@Test
+	void testPills() {
+		Purchase a = Purchase.parse("1 packet of headache pills at 9.75");
+		assertEquals("1 packet of headache pills: 9.75", a.getQuantity() + " " + a.getDescription() + ": " + a.getPricePurchase());		
+	}
+	/*
+	@Test
+	void testImportedEverywhere() {
+		Purchase a = Purchase.parse("3 box of imported chocolates at 11.25");
+		assertEquals("3 imported box of chocolates: 35.55", a.getQuantity() + " " + a.getDescription() + ": " + a.getPricePurchase());		
+	}
+	*/
 }
